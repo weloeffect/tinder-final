542a4f8968c55f41c37b47d9bb5b092a6700f50f
Walid Mohamed <walidridge1@gmail.com>
2022-10-17 11:48:03 +0300

    final commit

--- server.js
+++ server.js
@@ -38,9 +38,9 @@ app.use('/cards', cardRouter);
 //Listener 
 
 if (process.env.NODE_ENV === 'production'){
-    app.use(express.static('staff-frontend/build'))
+    app.use(express.static('frontend/build'))
     app.get('*', (req, res) =>{
-        res.sendFile(path.resolve(__dirname, 'staff-frontend', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     })
 }
 app.listen(port, () => {
